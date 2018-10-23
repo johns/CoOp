b55e7bd7b1b10dd9ef531de7e6100904c0081ef2
JohnS <johnrscott4@gmail.com>
2018-10-22 17:25:30 -0700

    Setup basic format and look of Home

--- src/client/config/setup.js
+++ src/client/config/setup.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {
     Text,
 } from 'react-native'
+import { colors } from "../lib/colors";
 import Home from "../screens/Home/Home";
 
 export default class Setup extends Component {
--- src/client/screens/Home/Home.js
+++ src/client/screens/Home/Home.js
@@ -1,14 +1,81 @@
 import React, { Component } from "react";
 import {
+    View,
     Text,
-} from 'react-native'
+    Button,
+    KeyboardAvoidingView,
+} from 'react-native';
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create ({
+  buttonBox: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+   goButton: {
+     paddingLeft: 20,
+     paddingRight: 20,
+     flex: 1,
+   },
+
+   groupButton: {
+     paddingLeft: 20,
+     paddingRight: 20,
+     paddingTop: 100,
+   },
+
+});
+
 
 export default class Home extends Component {
   render() {
     return (
-      <Text>
-      Test
-      </Text>
+      <KeyboardAvoidingView behavior="padding" enabled>
+
+        <View style={styles.buttonBox}>
+          <View style={styles.goButton}>
+            <Button
+              title="Sign Out"
+              color="#3e3f3c"
+              accessibilityLabel="Sign Out"
+            />
+          </View>
+          <View style={styles.goButton}>
+            <Button
+              title="Create Chat"
+              color="#70e74e"
+              accessibilityLabel="Create Chat"
+            />
+          </View>
+        </View>
+
+        <View style={styles.buttonBox}>
+          <View style={styles.groupButton}>
+              <Button
+                title="Group 1"
+                color="#94ad9a"
+                accessibilityLabel="Group 1"
+              />
+          </View>
+        </View>
+
+        <View style={styles.buttonBox}>
+          <View style={styles.groupButton}>
+              <Button
+                title="Group 2"
+                color="#94ad9a"
+                accessibilityLabel="Group 2"
+              />
+          </View>
+        </View>
+
+      </KeyboardAvoidingView>
     );
   }
 }
