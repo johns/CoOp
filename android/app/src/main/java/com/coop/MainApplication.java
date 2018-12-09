1c36ab391e588f19880c0b7e93bfe01f3f200bbe
Sam Chami <sc01914@gmail.com>
2018-12-09 12:45:11 -0800

    Changed packages over

--- android/app/src/main/java/com/coop/MainApplication.java
+++ android/app/src/main/java/com/coop/MainApplication.java
@@ -1,10 +1,10 @@
-package com.betterwe;
+package com.coop;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
